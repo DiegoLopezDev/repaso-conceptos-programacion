a0716df585ee07e56a81ba7017d8553b0a3f6892
Diego Lopez <diegolopez991108@gmail.com>
2020-11-04 19:44:15 -0500

    Se modifica el archivo index.js del ejemplo 1

---
+++ ejemplo-1/index.js
@@ -0,0 +1,26 @@
+//Comando para imprimir en consola
+console.log('Hola mundo');
+
+//Declaración de Variables
+let num1 = 1;
+let num2 = 5;
+let suma = num1 + num2;
+console.log(suma);
+
+let nombre = 'Diego';
+let apellido = 'López';
+let nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+nombreCompleto = `Nombre completo actualizado: ${nombre} ${apellido}`;
+console.log(nombreCompleto);
+
+//Operadores lógicos
+let edad1 = 10;
+let edad2 = 20;
+
+if (edad1 > edad2) {
+  console.log('la edad' + edad1 + ' es mayor que la edad ' + edad2);
+} else {
+  console.log('La edad ' + edad2 + ' es menor que la edad ' + edad1);
+}
