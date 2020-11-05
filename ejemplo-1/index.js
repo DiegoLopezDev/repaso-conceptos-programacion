ba90321f6e50145eda246d0250bac9c811bf414e
Mauricio Rivera <mauriciorivera@iMac-de-mac.local>
2020-11-04 20:24:40 -0500

    Se finaliza ejemplo de temperaturas con ifs

--- ejemplo-1/index.js
+++ ejemplo-1/index.js
@@ -12,6 +12,8 @@ let apellido = 'López';
 let nombreCompleto = nombre + ' ' + apellido;
 console.log(nombreCompleto);
 
+// Comillas simples ', comillas dobles ", acento grave `
+
 nombreCompleto = `Nombre completo actualizado: ${nombre} ${apellido}`;
 console.log(nombreCompleto);
 
@@ -26,3 +28,29 @@ if (edad1 > edad2) {
 }
 
 console.log('Este es un mensaje para subir a la nube');
+
+//Ejemplo de temperatura
+let temperatura = prompt('Escribe una temperatura', 0);
+
+// La temperatura es hiper caliente cuando es mayor a 40
+if (temperatura > 40) {
+  console.log('Me derrito del calor');
+}
+
+// Temperatura caliente es cuando estamos > a 20 hasta 40
+if (temperatura >= 20 && temperatura <= 40) {
+  console.log('Hace mucho calor');
+}
+
+// El número x está entre 2 y 8: 2 >= x <= 8
+// El número x es igual y mayor que 2 y menor e igual que 8: 2 >= x <= 8
+
+// Temperatura ni caliente ni frio es cuando estamos entre 19 y 12
+if (temperatura >= 12 && temperatura <= 19) {
+  console.log('La temperatura es estable');
+}
+
+// Temperatura fria es cuando estamos < 11
+if (temperatura <= 11) {
+  console.log('Hace mucho frío');
+}
